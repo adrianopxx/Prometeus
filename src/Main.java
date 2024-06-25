1bb4ec0fe3432710b24a53ecfbf34a9257e84d78
adrianopxx <adrianopxx@gmail.com>
2024-06-25 06:33:21 -0300

    teste versão 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame Jf = new JFrame();
+      Jf.setBounds(100,100,800,600);
+      Jf.setVisible(true);
     }
 }
